3019f6d012a4f2205e99ec832b5801b9c95187fd
ShunZhi Tang <tangshunzhi@zplay.com>
2019-08-20 15:12:15 +0800

    Implement yumi interstitial custom event

---
+++ AdMobAdapter-YumiAd-iOS/Interstitial/GADYUMICustomEventInterstitial.h
@@ -0,0 +1,18 @@
+//
+//  GADYUMICustomEventInterstitial.h
+//  AdMobAdapter-YumiAd-iOS
+//
+//  Created by Michael Tang on 2019/8/20.
+//  Copyright © 2019 MichaelTang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <GoogleMobileAds/GADMobileAds.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GADYUMICustomEventInterstitial : NSObject <GADCustomEventInterstitial>
+
+@end
+
+NS_ASSUME_NONNULL_END
